c8c6d1d8ee469b65d943db5a162a477352998104
PureCS <purecs@hotmail.com>
2017-04-21 22:35:22 +0100

    Update copyright header on files missing it

--- plugins/packets/CommandHandler.groovy
+++ plugins/packets/CommandHandler.groovy
@@ -1,3 +1,19 @@
+/*
+ * This file is part of RuneSource.
+ *
+ * RuneSource is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * RuneSource is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with RuneSource.  If not, see <http://www.gnu.org/licenses/>.
+ */
 import com.rs.entity.Position
 import com.rs.entity.player.Player
 import com.rs.entity.player.PlayerAttributes
